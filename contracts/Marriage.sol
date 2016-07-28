e94ba939573da196fd3eaae534fe47a2b978e330
Luke Sugiura <jun.sugiura.jp@gmail.com>
2016-07-28 11:45:35 +0900

    add accept method

--- contracts/Marriage.sol
+++ contracts/Marriage.sol
@@ -1,4 +1,6 @@
 contract Marriage {
+  enum PhaseOptions {Proposed, Married, Divorced}
+
   struct Partner {
     address wallet;
     uint initAmount;
@@ -7,27 +9,38 @@ contract Marriage {
   Partner partner0;
   Partner partner1;
 
+  PhaseOptions phase;
+
   function Marriage(address _wallet1, uint _initAmount1) {
     partner0 = Partner({wallet: msg.sender, initAmount: msg.value});
     partner1 = Partner({wallet: _wallet1, initAmount: _initAmount1});
+
+    phase = PhaseOptions.Proposed;
   }
 
   function edit() public {
     if(partner0.wallet == msg.sender) {
-      partner0.wallet.send(partner0.initAmount);
-      partner0.initAmount = 0;
-
+      partner0.wallet.send(this.balance);
       partner1.initAmount = msg.value;
     } else {
-      partner1.wallet.send(partner1.initAmount);
-      partner1.initAmount = 0;
-
+      partner1.wallet.send(this.balance);
       partner0.initAmount = msg.value;
     }
   }
 
   function accept() public {
+    if(msg.value != getInitAmount()) {
+      throw;
+    }
+    phase = PhaseOptions.Married;
+  }
 
+  function getInitAmount() returns(uint) {
+    if(partner0.wallet == msg.sender) {
+      return partner0.initAmount;
+    } else {
+      return partner1.initAmount;
+    }
   }
 
   function spend() public {
